d7db16cbe454998e25e8f8414f1a542d98f68088
Alex Fabiano <alex.fabiano.vix@gmail.com>
2023-07-22 22:05:07 -0300

    Arquivos do meu Portifolio

---
+++ assets/app/trocaImagemOutrasPaginas.js
@@ -0,0 +1,94 @@
+/* troca a imagem da estrela da logomarca */
+function trocarImagemEstrelaOutras() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaMaior--outras");
+    img.setAttribute("src", ".././assets/images/estrela-cor.png");
+}
+
+function voltarImagemEstrelaOutras() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaMaior--outras");
+    img.setAttribute("src", ".././assets/images/estrela.png")
+}
+
+
+/* troca a imagem da logo do e-mail  */
+function trocarImagemEmailOutras() {
+    var img = document.querySelector("#cabecalho__direita--logoEmailPb--outras");
+    img.setAttribute("src", ".././assets/images/logo-email-cor.jpg");
+}
+
+function voltarImagemEmailOutras() {
+    var img = document.querySelector("#cabecalho__direita--logoEmailPb--outras");
+    img.setAttribute("src", "../assets/images/logo-email-pb.png")
+}
+
+
+/* Troca Logo Github */
+function trocarImagemGithubOutras() {
+    var img = document.querySelector("#redesSociais__github--outras");
+    img.setAttribute("src", "../assets/images/sociais/logo-github-cor.png");
+}
+
+function voltarImagemGithubOutras() {
+    var img = document.querySelector("#redesSociais__github--outras");
+    img.setAttribute("src", "../assets/images/sociais/logo-github-pb.png")
+}
+
+
+/* Troca Logo Rede Social Linkedin */
+function trocarImagemLinkedinOutras() {
+    var img = document.querySelector("#redesSociais__linkedin--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-linkedin-cor.png");
+}
+
+function voltarImagemLinkedinOutras() {
+    var img = document.querySelector("#redesSociais__linkedin--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-linkedin-pb.png")
+}
+
+
+/* Troca Logo Rede Social Instagram */
+function trocarImagemInstagramOutras() {
+    var img = document.querySelector("#redesSociais__instagram--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-instagram-cor.png");
+}
+
+function voltarImagemInstagramOutras() {
+    var img = document.querySelector("#redesSociais__instagram--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-instagram-pb.png")
+}
+
+
+/* Troca Logo Rede Social Twitter */
+function trocarImagemTwitterOutras() {
+    var img = document.querySelector("#redesSociais__twitter--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-twitter-cor.png");
+}
+
+function voltarImagemTwitterOutras() {
+    var img = document.querySelector("#redesSociais__twitter--outras");
+    img.setAttribute("src", ".././assets/images/sociais/logo-twitter-pb.png")
+}
+
+
+/* troca Logo Curriculum */
+function trocarImagemCurriculumOutras() {
+    var img = document.querySelector("#redesSociais__curriculum--outras");
+    img.setAttribute("src", ".././assets/images/logo-curriculum-cor.png");
+}
+
+function voltarImagemCurriculumOutras() {
+    var img = document.querySelector("#redesSociais__curriculum--outras");
+    img.setAttribute("src", ".././assets/images/logo-curriculum-pb.png")
+}
+
+
+/* troca a imagem do Rodapé  */
+function trocarImagemRodapeOutras() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaRodape--outras");
+    img.setAttribute("src", ".././assets/images/estrela-cor.png");
+}
+
+function voltarImagemRodapeOutras() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaRodape--outras");
+    img.setAttribute("src", ".././assets/images/estrela.png")
+}
---
+++ assets/app/trocaImagemPaginaPrincipal.js
@@ -0,0 +1,94 @@
+/* troca a imagem da estrela da logomarca */
+function trocarImagemEstrela() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaMaior");
+    img.setAttribute("src", "./assets/images/estrela-cor.png");
+}
+
+function voltarImagemEstrela() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaMaior");
+    img.setAttribute("src", "./assets/images/estrela.png")
+}
+
+
+/* troca a imagem da logo do e-mail  */
+function trocarImagemEmail() {
+    var img = document.querySelector("#cabecalho__direita--logoEmailPb");
+    img.setAttribute("src", "./assets/images/logo-email-cor.jpg");
+}
+
+function voltarImagemEmail() {
+    var img = document.querySelector("#cabecalho__direita--logoEmailPb");
+    img.setAttribute("src", "./assets/images/logo-email-pb.png")
+}
+
+
+/* Troca Logo Github */
+function trocarImagemGithub() {
+    var img = document.querySelector("#redesSociais__github");
+    img.setAttribute("src", "./assets/images/sociais/logo-github-cor.png");
+}
+
+function voltarImagemGithub() {
+    var img = document.querySelector("#redesSociais__github");
+    img.setAttribute("src", "./assets/images/sociais/logo-github-pb.png")
+}
+
+
+/* Troca Logo Rede Social Linkedin */
+function trocarImagemLinkedin() {
+    var img = document.querySelector("#redesSociais__linkedin");
+    img.setAttribute("src", "./assets/images/sociais/logo-linkedin-cor.png");
+}
+
+function voltarImagemLinkedin() {
+    var img = document.querySelector("#redesSociais__linkedin");
+    img.setAttribute("src", "./assets/images/sociais/logo-linkedin-pb.png")
+}
+
+
+/* Troca Logo Rede Social Instagram */
+function trocarImagemInstagram() {
+    var img = document.querySelector("#redesSociais__instagram");
+    img.setAttribute("src", "./assets/images/sociais/logo-instagram-cor.png");
+}
+
+function voltarImagemInstagram() {
+    var img = document.querySelector("#redesSociais__instagram");
+    img.setAttribute("src", "./assets/images/sociais/logo-instagram-pb.png")
+}
+
+
+/* Troca Logo Rede Social Twitter */
+function trocarImagemTwitter() {
+    var img = document.querySelector("#redesSociais__twitter");
+    img.setAttribute("src", "./assets/images/sociais/logo-twitter-cor.png");
+}
+
+function voltarImagemTwitter() {
+    var img = document.querySelector("#redesSociais__twitter");
+    img.setAttribute("src", "./assets/images/sociais/logo-twitter-pb.png")
+}
+
+
+/* troca Logo Curriculum */
+function trocarImagemCurriculum() {
+    var img = document.querySelector("#redesSociais__curriculum");
+    img.setAttribute("src", "./assets/images/logo-curriculum-cor.png");
+}
+
+function voltarImagemCurriculum() {
+    var img = document.querySelector("#redesSociais__curriculum");
+    img.setAttribute("src", "./assets/images/logo-curriculum-pb.png")
+}
+
+
+/* troca a imagem do Rodapé  */
+function trocarImagemRodape() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaRodape");
+    img.setAttribute("src", "./assets/images/estrela-cor.png");
+}
+
+function voltarImagemRodape() {
+    var img = document.querySelector("#cabecalho__esquerda--estrelaRodape");
+    img.setAttribute("src", "./assets/images/estrela.png")
+}
